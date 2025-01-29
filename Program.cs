daaca40de6f8c682cf0558b72a2a7ddb79ca4124
Aperson <69006803+aperson5647@users.noreply.github.com>
2025-01-29 19:49:18 +0000

    Updates

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using NetCord;
+using NetCord.Gateway;
+
+GatewayClient client = new(new BotToken("MTIyNDQzNTM4NjAyNjI5NTMzNw.Gs_g8s.czVnzV8Vkwi-ETO1-Cs0aVlOpX8nyfRdSNz2sk"));
+
+client.Log += message =>
+{
+    Console.WriteLine(message);
+    return default;
+};
+
+await client.StartAsync();
+await Task.Delay(-1);
