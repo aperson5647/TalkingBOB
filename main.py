d900dbd6be8a7266b600b3faf1a9d7fb6189dd28
Aperson <69006803+aperson5647@users.noreply.github.com>
2025-02-01 21:04:42 +0000

    MOVED TO PYTHON

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+print("hi")
+def poo(name):
+    print(name)
+
+while True:
+    fart = random.randint(0,5)
+    if fart == 0:
+        poo("shit")
+    elif fart == 1:
+        poo("pee")
+    elif fart == 2:
+        poo("shit v3.0")
+    elif fart == 3:
+        poo("fart")
+    elif fart == 4:
+        poo(fart)
+    elif fart == 5:
+        poo("shit version 2")
